ccd6647601ed6c4a3ddc7515790a1b78e052db66
ndongamadu <ndongamadou@gmail.com>
2021-08-12 18:56:56 +0000

    region filter and colors

--- site.js
+++ site.js
@@ -1,7 +1,9 @@
 let data_url = 'data.csv';
 let countriesArr = [],
+    regionsArr = ['All'],
     tagsArr = [];
-
+let mainTags = ["Information","Knowledge","Perception","Practice"];
+let altTags = ["Social environment", "Structural factor"];
 
 $(document).ready(function(){
     var data_all ;
@@ -11,37 +13,44 @@ $(document).ready(function(){
             d3.csv(data_url)
         ]).then(function(d){
             data_all = d[0]; 
-            // console.log(data);
             data_all.forEach(element => {
                 // var href = '<a href="'+element['link']+'" target="blank">Link</a>';
                 // element['link'] = href; 
                 var arr = element['dimension'].split(",");
                 var trimedArr = arr.map(x => x.trim());
-                var formatedDims = [];
+                var formatedDims = "";
                 trimedArr.forEach(d => {
-                    formatedDims.push('<label class="alert tag">'+d+'</label>');
+                    mainTags.includes(d) ? formatedDims +='<label class="alert tag-main">'+d+'</label>' : formatedDims +='<label class="alert tag-alt">'+d+'</label>';
                 });
                 element['formattedDimension'] = formatedDims;
                 var arr = element['insert_date'].split(" ");
                 element['insert_date'] = arr[0];
             });
-            console.log(d[0]);
             console.log(data_all);
+
             d[0].forEach(element => {
                 var pays = element['countries'].split(",");
                 var dims = element['dimension'].split(",");
+                var regs = element['region'].split(",");
                 pays.forEach(p => {
                     countriesArr.includes(p.trim()) ? '' : countriesArr.push(p.trim());
                 });
                 dims.forEach(d => {
                     tagsArr.includes(d.trim()) ? '' : tagsArr.push(d.trim());
                 });
+                regs.forEach(r => {
+                    regionsArr.includes(r.trim()) ? '' : regionsArr.push(r.trim());
+                });
             });
+
+            // 
+            generateRegionSelect();
+
             // console.log("countries: " + countriesArr);
-            // console.log("tags: " + tagsArr);
+            // console.log("regions: " + regionsArr);
             var dtData = [];
             data_all.forEach(element => {
-                dtData.push([element['source_id'],element['title'], element['organisation'],element['formattedDimension'], element['country_count'],element['insert_date'], element['publication_channel']]);
+                dtData.push([element['source_id'],element['title'],element['formattedDimension'], element['region'],element['source_date'], element['organisation'],element['publication_channel']]);
             });
             console.log(dtData);
             var datatable = $('#datatable').DataTable({
@@ -56,8 +65,8 @@ $(document).ready(function(){
                     },
                     {"width": "25%"},
                     {"width": "15%"},
-                    {"width": "25%"},
                     {"width": "15%"},
+                    {"width": "5%"},
                     {"width": "10%"},
                     {"width": "5%"}
                 ],
@@ -65,11 +74,15 @@ $(document).ready(function(){
                     {
                         "className": "dt-head-left",
                         "targets": "_all"
-                    }
+                    },
+                    { "searchable" : true, "targets": "_all"}
                 ],
-                "scrollY": "600px", 
-                "scrollCollapse": true,
-                "paging": false,
+                // "scrollY": "600px", 
+                // "scrollCollapse": true,
+                // "paging": true,
+                "pageLength": 20,
+                "bLengthChange": false,
+                "pagingType": "simple_numbers",
                 "order":[[1, 'asc']],
                 "dom": "lrtp",
                 // "createdRow": function( row, data, dataIndex ) {
@@ -96,55 +109,71 @@ $(document).ready(function(){
                             '<td>'+
                                 '<table class="tabDetail" >'+
                                     '<tr>'+
-                                        '<th rowspan="2"><strong>Geo</strong></th>'+
+                                        // '<th rowspan="2"><strong>Geo</strong></th>'+
+                                        '<th><strong>Geo</strong></th>'+
                                         '<td>Region</td>'+
                                         '<td>'+filtered[0]['region']+'</td>'+
                                     '</tr>'+
                                     '<tr>'+
+                                        '<td>&nbsp;</td>'+
                                         '<td>Countries ('+filtered[0]['country_count']+')</td>'+
                                         '<td>'+filtered[0]['countries']+'</td>'+
                                     '</tr>'+
                                 
                                     '<tr>'+
-                                        '<th rowspan="3"><strong>Purpose</strong></th>'+
+                                        // '<th rowspan="3"><strong>Purpose</strong></th>'+
+                                        '<th><strong>Purpose</strong></th>'+
                                         '<td>Summary</td>'+
                                         '<td>'+filtered[0]['details']+'</td>'+
                                     '</tr>'+
                                     '<tr>'+
+                                        '<td>&nbsp;</td>'+
                                         '<td>Indicators</td>'+
                                         '<td>'+filtered[0]['variables']+'</td>'+
                                     '</tr>'+
                                     '<tr>'+
+                                        '<td>&nbsp;</td>'+
                                         '<td>Target</td>'+
                                         '<td>'+filtered[0]['target_pop']+'</td>'+
                                     '</tr>'+
                                     '<tr>'+
-                                        '<th rowspan="4"><strong>Method</strong></th>'+
+                                        // '<th rowspan="4"><strong>Method</strong></th>'+
+                                        '<th><strong>Method</strong></th>'+
                                         '<td>Survey</td>'+
                                         '<td>'+filtered[0]['methodology']+'</td>'+
                                     '</tr>'+
                                     '<tr>'+
+                                        '<td>&nbsp;</td>'+
                                         '<td>Sample</td>'+
-                                        '<td>'+filtered[0]['sample_type']+'</td>'+
-                                        '<td>'+filtered[0]['sample_size']+' respondents</td>'+
+                                        '<td>'+filtered[0]['sample_type']+' - '+filtered[0]['sample_size']+' respondents</td>'+
+                                        // '<td>'+filtered[0]['sample_size']+' respondents</td>'+
                                     '</tr>'+
                                     '<tr>'+
+                                        '<td>&nbsp;</td>'+
                                         '<td>Review</td>'+
                                         '<td>'+filtered[0]['quality_check']+'</td>'+
                                     '</tr>'+
                                     '<tr>'+
+                                        '<td>&nbsp;</td>'+
                                         '<td>Comment</td>'+
                                         '<td>'+filtered[0]['source_comment']+'</td>'+
                                     '</tr>'+
                                     '<tr>'+
-                                        '<th rowspan="2"><strong>Source</strong></th>'+
+                                        // '<th rowspan="3"><strong>Source</strong></th>'+
+                                        '<th><strong>Source</strong></th>'+
                                         '<td>Data Type</td>'+
                                         '<td>'+filtered[0]['access_type']+'</td>'+
-                                        '<td>'+filtered[0]['publication_channel']+ ' (<a href="'+filtered[0]['link']+'" target="blank">link</a>)</td>'+
                                     '</tr>'+
                                     '<tr>'+
+                                        '<td>&nbsp;</td>'+
                                         '<td>Authors</td>'+
                                         '<td>'+filtered[0]['authors']+'</td>'+
+                                    '</tr>'+
+                                    '<tr>'+
+                                        '<td>&nbsp;</td>'+
+                                        '<td>Publication</td>'+
+                                        '<td><a href="'+filtered[0]['link']+'" target="blank"><i class="fa fa-download fa-sm"></i></a></td>'+
+                                        // '<td>'+filtered[0]['publication_channel']+'<a href="'+filtered[0]['link']+'" target="blank"><i class="fa fa-download fa-sm"></i></a></td>'+
                                     '</tr>'+
                                 
                                 '</table>'+
@@ -194,7 +223,7 @@ $(document).ready(function(){
         var arr = (dataArg == undefined ? data_all : dataArg);
         var dt = [];
         arr.forEach(element => {
-            dt.push([element['source_id'],element['title'], element['organisation'],element['formattedDimension'], element['country_count'],element['insert_date'], element['publication_channel']]);
+            dt.push([element['source_id'],element['title'],element['formattedDimension'], element['region'],element['source_date'], element['organisation'],element['publication_channel']]);
         });
         $('#datatable').dataTable().fnClearTable();
         $('#datatable').dataTable().fnAddData(dt);
@@ -204,6 +233,15 @@ $(document).ready(function(){
         $('#keyFigs').html("");
 
     }
+
+    function generateRegionSelect(){
+        var options = '';
+        for (let i = 0; i < regionsArr.length; i++) {
+        i == 0 ? options += '<option value="' + regionsArr[i] + '" selected>' + regionsArr[i] + '</option>' :
+            options += '<option value="' + regionsArr[i] + '">' + regionsArr[i] + '</option>';
+        }
+        $('#regionSelect').html(options);
+    } //generateRegionSelect
 
     function createButtons(){
         var html = "";
@@ -233,9 +271,26 @@ $(document).ready(function(){
             })
             updateTable(filter);
         }
-
         
-    }
+    } //clickButton
+
+    $('#regionSelect').on('change', function(){
+        var regionSelected = $('#regionSelect').val();
+        console.log("region selected: " +regionSelected);
+
+        if (regionSelected == "All") {
+            updateTable();
+        } else {
+            var filter = data_all.filter(function(d) {
+                var arr = d['region'].split(",");
+                var trimedArr = arr.map(x => x.trim());
+                return (trimedArr.includes(regionSelected) ? d : null);
+            });
+
+            updateTable(filter);
+        }
+
+    });
 
 
 
